d26bf99ba785b87e1e4852a48f0df4d0b29d17f4
Aaron Karp <aaronkarp@outlook.com>
2024-05-25 10:00:09 -0400

    add UserList component

---
+++ src/components/UserList.js
@@ -0,0 +1,26 @@
+import { makeStyles, shorthands, tokens } from '@fluentui/react-components';
+import { useTasks } from '../contexts/TasksContext';
+import User from './User';
+
+const useStyles = makeStyles({
+  list: {
+    listStyleType: 'none',
+    padding: 0,
+    width: '100%',
+    ...shorthands.margin(tokens.spacingVerticalL, 0)
+  }
+});
+function UserList() {
+  const { users } = useTasks();
+  const classes = useStyles();
+
+  return (
+    <ul className={classes.list}>
+      {users.map((user) => (
+        <User user={user} key={user.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default UserList;
